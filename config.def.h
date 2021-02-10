8e89e1c5c43bc02269dc6739abb849c6e3192c99
Antonio Ochoa <antonhub00@gmail.com>
2021-02-10 03:13:22 -0600

    Rules updated

--- config.def.h
+++ config.def.h
@@ -29,7 +29,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-    { NULL,       NULL,       NULL,       0,            0,       -1 },
+    /* { NULL,       NULL,       NULL,       0,            0,       -1 }, */
+    { "Gnome-screenshot",       NULL,       NULL,       0,            1,       -1 },
 };
 
 /* layout(s) */
